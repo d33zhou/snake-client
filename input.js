30c8684fd53f91a4187df16610e32003a4dc99a2
d33zhou <d33zhou@outlook.com>
2021-12-01 19:09:25 +0000

    link connection object to input module and write movement commands to server

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
+
+// Stores the active TCP connection object.
+let connection
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn; // object to allow interaction with the server
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,10 +14,27 @@ const setupInput = function () {
   return stdin;
 };
 
+
 // actioons to perform given user key inputs
 const handleUserInput = function (key) {
   if (key === '\u0003') { // ctrl+c to terminate the game
     process.exit();
+  }
+
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+
+  if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
--- play.js
+++ play.js
@@ -4,6 +4,4 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-connect();
-
-setupInput();
+setupInput(connect());
